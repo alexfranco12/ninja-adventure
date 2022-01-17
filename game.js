d89a449789fc29785eb4f13e7e0f4bdaa66fc80c
alfranc <alexfranco12@gmail.com>
2022-01-17 15:43:55 -0600

    add matter plugin to assist with collisions later on. Make the game responsive to window size.

--- game.js
+++ game.js
@@ -1,16 +1,65 @@
 import overworld from './src/overworld.js'
 
+let game;
+const gameOptions = {
+  width: 60 * 16,
+  height: 40 * 16,
+  aspectRatio: 16/9,
+}
+
 var config = {
   type: Phaser.AUTO,
-  width: 80 * 16,
-  height: 50 * 16,
-  scene: [overworld],
+  width: gameOptions.width,
+  height: gameOptions.height,
+  background: '#000',
+  scene: [ overworld ],
+  scale: {
+    parent: 'game-container',
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+    zoom: 1,
+  },
   physics: {
-    default: 'arcade',
-    arcade: {
+    default: 'matter',
+    matter: {
       gravity: { y: 0 }
     }
   },
+  // Install the scene plugins
+  plugins: {
+    scene: [
+      {
+        /** 
+         *  Note! If you are including the library via the CDN script tag, the plugin line should be: 
+         *  plugin: PhaserMatterCollisionPlugin.default
+         */
+        plugin: PhaserMatterCollisionPlugin.default, // plugin class
+        key: "matterCollision",
+        mapping: "matterCollision"
+      }
+    ]
+  },
 };
 
-const game = new Phaser.Game(config);
+window.onload = () => {
+  game = new Phaser.Game(config);
+  window.focus();
+  resizeGame();
+  window.addEventListener('resize', resizeGame)
+}
+
+// resize the game screen
+function resizeGame(){
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+
+  if(windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+  } else{
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
