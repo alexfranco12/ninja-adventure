097443b70f1aa7bc73438f6ee23dc3296408ca60
Alexander Franco <alexander.franco@teladochealth.com>
2022-11-20 18:11:18 -0600

    add animation to the hero's walking

--- src/hero.js
+++ src/hero.js
@@ -1,12 +1,11 @@
-
+import { createAnimations } from './animations.js'
 
 class Hero extends Phaser.Physics.Matter.Sprite {
   constructor(config) {
     const { scene, x, y, texture, frame } = config
     super(scene.matter.world, x, y, texture, frame)
     this.scene = scene;
-    this.x = x;
-    this.y = y;
+    this.name = 'hero'
 
     this.init(this)
   }
@@ -24,6 +23,10 @@ class Hero extends Phaser.Physics.Matter.Sprite {
   init(hero) {
     this.scene.add.existing(hero)
 
+    // hero properties
+    this.setOrigin(.5, 1)
+    this.setDepth(2)
+
     // hero movement keys ( w, s, d, a )
     hero.inputKeys = this.scene.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -31,6 +34,9 @@ class Hero extends Phaser.Physics.Matter.Sprite {
       right: Phaser.Input.Keyboard.KeyCodes.D,
       left: Phaser.Input.Keyboard.KeyCodes.A,
     })
+
+    // Animation set
+    createAnimations(this.scene.anims, this.name)
   }
 
   update() {
@@ -38,14 +44,18 @@ class Hero extends Phaser.Physics.Matter.Sprite {
     // hero movement
     if (this.inputKeys.up.isDown) {
       this.y -= 1
+      this.play(`${this.name}-walk-up`, true)
     } else if (this.inputKeys.down.isDown) {
       this.y += 1
+      this.play(`${this.name}-walk-down`, true)
     } else if (this.inputKeys.left.isDown) {
       this.x -= 1
+      this.play(`${this.name}-walk-left`, true)
     } else if (this.inputKeys.right.isDown) {
       this.x += 1
+      this.play(`${this.name}-walk-right`, true)
     } else {
-
+      this.stop()
     }
   }
 
